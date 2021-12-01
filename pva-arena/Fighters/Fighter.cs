03a3d5dfea46f647ed8ab1a07fbf44d0d557c7e4
melancholy.txt <tovisnak@gmail.com>
2021-12-01 11:33:16 +0100

    Added basic classes + inheritance

---
+++ pva-arena/Fighters/Fighter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pva_arena
+{
+    class Fighter
+    {
+        public int HP { get; set; }
+        public int DMG { get; set; }
+        public int DodgeChance { get; set; }
+        public string Name { get; set; }
+
+    }
+}
